13e9113e2e6cd832932bf20d1600a7f313c9f30d
Nandakishore695 <104244494+Nandakishore695@users.noreply.github.com>
2022-08-08 09:31:10 +0530

    Update Bascis.js

--- Bascis2/Bascis.js
+++ Bascis2/Bascis.js
@@ -3,9 +3,9 @@ function valueToFixed() {
     let value2 = document.getElementById("twoFixed").value;
     // debugger
     if (value1 == "" || value2 == "") {
-      window.alert("please Enter value");
+      document.getElementById("error").innerHTML = "invalid";
     }
-  
     let fixedValue = value1.toFixed(value2);
     document.getElementById("result1").value = fixedValue;
+    document.getElementById("result1").value = '';
   }
