2cc9f0f77ec2a3c7d6574dab19d8d40ecb15f515
Nandakishore Vishwanath <104244494+Nandakishore695@users.noreply.github.com>
2022-12-30 09:47:48 +0530

    Update tableindx.js

--- JavascriptCurdOperation/tableindx.js
+++ JavascriptCurdOperation/tableindx.js
@@ -1,126 +1,110 @@
-const studentsSec = document.querySelector(".student-form");
-const gridSec = document.querySelector(".grid");
-const btnEd = document.querySelector("#btnEdit");
-const btnDl = document.querySelector("#btnDelete");
-const btnAdd = document.querySelector("#btnAdd");
-const btnSubmit = document.querySelector("#btnSubmit");
-const btnCancel = document.querySelector("#btnCancel");
-btnEd.addEventListener("click", editStudent);
-btnDl.addEventListener("click", deleteStudent);
-btnAdd.addEventListener("click", addStudent);
-btnSubmit.addEventListener("click", saveStudent);
-btnCancel.addEventListener("click", cancelStudent);
-let isEdit = false;
-const studentsList = [
-    { name: "John", email: "John@gmail.com ", gender: "Male", mobile: "546456456", id: 1 },
-    { name: "Doe", email: "doe@gmail.com", gender: "Male", mobile: 34534434554, id: 2 }
-]
+const employes = [{ id: 1, name: "NandaKishore", email: "nandakishore@gmail.com", gender: "male", mobile: '7893797371', },
+{ id: 2, name: "Upender", email: "upendere@gmail.com", gender: "male", mobile: '990894535', },
+{ id: 3, name: "Ajay", email: "ajay@gmail.com", gender: "male", mobile: '880894535', }
+];
 
-let selection = [];
-renderStudents();
-function renderStudents() {
-    selection = [];
+let redcordSelected = [];
+function renderEmployesList(emp) {
+    const listOfRow = `<tr>
+    <td><input type="checkbox" id="${emp.id}"/></td>
+    <td>${emp.name}</td>
+    <td>${emp.email}</td>
+    <td>${emp.gender}</td>
+    <td>${emp.mobile}</td>
+    </tr>`
+    return listOfRow;
+}
+
+function tableListData() {
+    redcordSelected = [];
     let tbody = document.querySelector("#tbStudents");
-    tbody.innerHTML = "";
-    studentsList.forEach(function (item) {
-        tbody.innerHTML += renderRow(item)
-    });
-    tbody.querySelectorAll("input").forEach(function (element) {
-        element.addEventListener("click", handleSelection);
+    tbody.innerHTML = ""
+    employes.forEach(function (para) {
+        tbody.innerHTML += renderEmployesList(para)
+    })
+    tbody.querySelectorAll("input").forEach(function (checkList) {
+        checkList.addEventListener("click", checkboxSelection);
     })
 }
-function handleSelection(event) {
-    const selectedEle = event.target;
-    const id = selectedEle.dataset.id;
-    if (selectedEle.checked) {
-        selection.push(id);
-    } 
-    // else {
-    //     const idx = selection.indexOf(id);
-    //     if (idx > -1) {
-    //         selection.splice(idx, 1)
-    //     }
-    // }
-}
-function renderRow(student) {
-    const _tbRow = `<tr>
-   <td><input type="checkbox" data-id="${student.id}" /></td>
-   <td>${student.name}</td>
-   <td>${student.email}</td>
-   <td>${student.gender}</td>
-   <td>${student.mobile}</td>
-   </tr>`
-    return _tbRow;
-}
-function editStudent() {
-    isEdit = true;
-    const student = studentsList.find(function (std) { return std.id == selection[0] });
-    gridSec.hidden = true;
-    studentsSec.hidden = false;
-    const _form = document.forms["studentForm"];
-    _form.name.value = student.name;
-    _form.email.value = student.email;
-    _form.gender.value = student.gender;
-    _form.mobile.value = student.mobile;
-}
-function addStudent() {
-    isEdit = false;
-    studentsSec.hidden = false;
-    gridSec.hidden = true;
-}
-function saveStudent() {
-    const _form = document.forms["studentForm"];
-    const student = {
-        id: "",
-        name: _form.name.value,
-        email: _form.email.value,
-        gender: _form.gender.value,
-        mobile: _form.mobile.value
-    }
-    if (student.name == "") {
-        const errorSpan = document.querySelector(".error_name").hidden = false;
-    }
-    else if (student.email === "") {
+tableListData();
 
+function checkboxSelection(event) {
+    const idSelection = event.target;
+    const id = idSelection.id;
+    if (idSelection.checked) {
+        redcordSelected.push(id)
     }
-    else if (student.email) {
-        if (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(student.email)) {
+}
 
-        } else {
-            alert("Please enter valid email")
+const btnDelete = document.querySelector("#btnDelete");
+btnDelete.addEventListener("click", deleteDataOfEmployes);
+function deleteDataOfEmployes() {
+    if (redcordSelected.length > 1) {
+        alert("Please select one student");
+    }
+    else {
+        const authenticate = confirm("sure to delete selected record");
+        if (authenticate) {
+            const idx = employes.findIndex(function (value) {
+                return value.id == redcordSelected[0];
+            })
+            employes.splice(idx, 1);
+            tableListData()
         }
     }
+}
+
+const savebtn = document.querySelector("#btnSubmit")
+savebtn.addEventListener("click", newRecordData)
+function newRecordData() {
+    debugger
+    const insertData = document.forms["employeDataCreating"]
+    const obj = {
+        id: "",
+        name: insertData.name.value,
+        email: insertData.email.value,
+        gender: insertData.gender.value,
+        mobile: insertData.mobile.value,
+    };
+    obj.id = employes.length + 1;
+    employes.push(obj);
     if (isEdit) {
-        studentsList.forEach(function (item) {
-            if (item.id == selection[0]) {
-                item.name = student.name;
-                item.email = student.email;
-                item.gender = student.gender;
-                item.mobile = student.mobile;
+        employes.forEach(function (item) {
+            if (item.id == redcordSelected[0]) {
+                item.name = obj.name;
+                item.email = obj.email;
+                item.gender = obj.gender;
+                item.mobile = obj.mobile;
             }
         })
-    } else {
-        student.id = studentsList.length + 1;
-        studentsList.push(student);
     }
-    studentsSec.hidden = true;
-    gridSec.hidden = false;
-    renderStudents();
+    tableListData()
+    formReset();
 }
-function cancelStudent() {
 
+let isEdit = false;
+const btnEditEmploye = document.querySelector("#btnEdit");
+btnEditEmploye.addEventListener("click", editEmploye);
+function editEmploye() {
+    isEdit = true;
+    const employesFind = employes.find(function (item) {
+        return item.id == redcordSelected[0]
+    })
+    const employesDetailsEdit = document.forms["employeDataCreating"]
+    employesDetailsEdit.name.value = employesFind.name;
+    employesDetailsEdit.email.value = employesFind.email;
+    employesDetailsEdit.gender.value = employesFind.gender;
+    employesDetailsEdit.mobile.value = employesFind.mobile;
 }
-function deleteStudent() {
-    debugger
-    if (selection.length > 1 || selection.length === 0) {
-        alert("Please select one student");
-    } else {
-        const isConfirm = confirm("Are you sure ? do you really want to delete ?");
-        if (isConfirm) {
-            debugger
-            const idx = studentsList.findIndex(function (value) { return value.id == selection[0] })
-            studentsList.splice(idx, 1);
-            renderStudents();
-        }
-    }
+
+function formReset() {
+    const insertData = document.forms["employeDataCreating"]
+    const obj = {
+        id: "",
+        name: insertData.name.value = " ",
+        email: insertData.email.value = " ",
+        gender: insertData.gender.value = " ",
+        mobile: insertData.mobile.value = " ",
+    };
+    obj = null
 }
